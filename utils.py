6fc532a2e818a892eb6b20ee3359173d6ee693fc
scrpr <46473171+scrpr@users.noreply.github.com>
2024-04-03 13:04:59 +0800

    workaround to protect fontname from converting

--- utils.py
+++ utils.py
@@ -17,12 +17,13 @@ def cleanup_ass_file(input_file, output_file):
         with open(output_file, encoding='utf-8-sig', mode='w+') as f_out:
             subs.dump_file(f_out)
 
-def traditionalize_text(input_text, user_pre_replace="", timeout=10, max_tries=3):
+def traditionalize_text(input_text, user_pre_replace="", user_protect_replace="", timeout=10, max_tries=3):
     url = "https://api.zhconvert.org/convert"
     data = {
         "text": input_text,
         "converter": "Taiwan",
-        "userPreReplace": user_pre_replace
+        "userPreReplace": user_pre_replace,
+        "userProtectReplace": user_protect_replace
     }
     
     retry_strategy = Retry(total=max_tries, backoff_factor=0.3)
@@ -42,7 +43,7 @@ def traditionalize_text(input_text, user_pre_replace="", timeout=10, max_tries=3
     
     raise Exception("Error: Maximum number of tries exceeded")
 
-def traditionalize_ass(input_file, output_file, user_pre_replace=""):
+def traditionalize_ass(input_file, output_file, user_pre_replace="", user_protect_replace=""):
     with open(input_file, encoding='utf-8-sig', mode='r') as f:
         doc = ass.parse(f)
         new_texts = []
@@ -54,7 +55,7 @@ def traditionalize_ass(input_file, output_file, user_pre_replace=""):
             print(f"Traditionalizing line {i} to {i+50}...")
             slice_texts = temp_texts[i:i+50]
             slice_texts = json.dumps(slice_texts, ensure_ascii=False)
-            traditionalized_slice = traditionalize_text(slice_texts, user_pre_replace)
+            traditionalized_slice = traditionalize_text(slice_texts, user_pre_replace, user_protect_replace)
             new_texts += json.loads(traditionalized_slice)
         for i in range(len(new_texts)):
             doc.events[i].text = new_texts[i].replace("思源黑体", "Source Han Sans TC").replace("思源宋体", "Source Han Serif TC").replace("Source Han Sans SC", "Source Han Sans TC").replace("Source Han Serif SC", "Source Han Serif TC").replace("{\\fn思源黑体\\b1\\fs70\\fscx105}䌷","䌷")
@@ -102,7 +103,7 @@ def traverse_files(folder_path):
                     continue
 
                 cleanup_ass_file(input_file, output_file)
-                traditionalize_ass(output_file, output_tc_file, user_pre_replace='艾米莉=艾蜜莉\n斯图亚特=司徒亚特\n试镜=甄选会\n埃琳娜=艾琳娜')
+                traditionalize_ass(output_file, output_tc_file, user_pre_replace='艾米莉=艾蜜莉\n斯图亚特=司徒亚特\n试镜=甄选会\n埃琳娜=艾琳娜', user_protect_replace='华康翩翩体\n獅尾圓體')
 
 def merge_files(input_file, output_file):
     with open(input_file, encoding='utf-8-sig', mode='r') as f:
